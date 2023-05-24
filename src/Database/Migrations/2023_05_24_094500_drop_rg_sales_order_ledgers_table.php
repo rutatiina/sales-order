b628e481a330a04b423841e808718ea9a12e0083
Gilbert Rutatiina <rutatiina@gmail.com>
2023-05-24 09:59:11 +0300

    DYNAMIC LEDGER INFO GENERATION - update

--- src/Database/Migrations/2023_05_24_094500_drop_rg_sales_order_ledgers_table.php
+++ src/Database/Migrations/2023_05_24_094500_drop_rg_sales_order_ledgers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DropRgRetainerInvoiceLedgersTable extends Migration
+class DropRgSalesOrderLedgersTable extends Migration
 {
     /**
      * Run the migrations.
